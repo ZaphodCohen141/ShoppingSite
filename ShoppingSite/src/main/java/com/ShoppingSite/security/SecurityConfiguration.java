be262adb3516f323b6311f43fd91f402c90e3d0d
zaphdCohen141 <ayelet.co92@gmail.com>
2024-09-01 22:23:31 +0300

    final commit

---
+++ ShoppingSite/src/main/java/com/ShoppingSite/security/SecurityConfiguration.java
@@ -0,0 +1,4 @@
+package com.ShoppingSite.security;
+
+public class SecurityConfiguration {
+}
---
+++ ShoppingSite/src/main/java/com/ShoppingSite/utils/ApiImageParams.java
@@ -0,0 +1,8 @@
+package com.ShoppingSite.utils;
+
+public class ApiImageParams {
+    public static final String APP_ID = "647051";
+    public static final String ACCESS_KEY = "R1w8WKgNBk6h9_OQSBxcBPso6_tH1QKnM7tfYeCWXPQ";
+    public static final  String SECRET_KEY = "5yaGwFU542i0Y9getJoc6vC5Mu-pwkOVMV9NKc1DUfw";
+
+}
